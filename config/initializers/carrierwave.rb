3e87f5cce91ab88349d8874d6a01a4dff8f19dcc
activeliang <activeliang@gmail.com>
2017-09-24 15:05:53 +0800

    change config method

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = Rails.application.qiniu.qiniu_access_key
-  config.qiniu_secret_key    = Rails.application.qiniu.qiniu_secret_key
-  config.qiniu_bucket        = Rails.application.qiniu.qiniu_bucket
-  config.qiniu_bucket_domain = Rails.application.qiniu.qiniu_bucket_domain
+  config.qiniu_access_key    = Figaro.env.qiniu_access_key
+  config.qiniu_secret_key    = Figaro.env.qiniu_secret_key
+  config.qiniu_bucket        = Figaro.env.qiniu_bucket
+  config.qiniu_bucket_domain = Figaro.env.qiniu_bucket_domain
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up-z1.qiniu.com"  #选择不同的区域时，"up.qiniu.com" 不同
