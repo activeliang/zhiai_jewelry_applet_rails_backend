9773d620e488d1e73478e9c2bdbd12832559d65a
huanghongliang <activeliang@gmail.com>
2018-01-22 20:20:35 +0800

    chnage deploy branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-set :branch, "ss_2"
+set :branch, "ss_test"
 
 server "39.108.106.162", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
